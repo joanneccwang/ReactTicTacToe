cb62c2c94d79aa7a1bc7715909ab657a10b77871
JoanneWang <joanneccwang@gmail.com>
2019-08-11 22:44:38 +0800

    FEATURE: Take turns and decide the winner

--- src/index.js
+++ src/index.js
@@ -18,13 +18,20 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      XIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice(); // shallow copy squares;
-    squares[i] = 'X'; // change target value
-    this.setState({squares: squares});
+    if (calculateWinner(this.state.squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.XIsNext ? 'X': 'O'; // change target value
+    this.setState({
+      squares: squares,
+      XIsNext: !this.state.XIsNext,   // flip 'X', 'O'
+    });
   }
 
   renderSquare(i) {
@@ -33,8 +40,15 @@ class Board extends React.Component {
       onClick={() => this.handleClick(i)}/>;
   }
 
+  // render is called whenever value is changed
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner is ${winner}`;
+    } else {
+      status = `Next player: ${this.state.XIsNext ? 'X' : 'O'}`;
+    }
 
     return (
       <div>
@@ -81,3 +95,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
